00fa9498fc68484a0527768ebaefd5032f647395
Shantanu Raj <s@sraj.me>
2022-12-24 06:18:43 +0100

    Highlight TODOs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,83 +1,94 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Theme
-  use 'folke/tokyonight.nvim'
+	-- Theme
+	use("folke/tokyonight.nvim")
 
-  -- GitHub CoPilot
-  use 'github/copilot.vim'
+	-- GitHub CoPilot
+	use("github/copilot.vim")
 
-  -- Fzf native
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = ' arch -arm64 make' }
+	-- Fzf native
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = " arch -arm64 make" })
 
-  -- Telescope
-  use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = {
-      'nvim-lua/plenary.nvim',
-    },
-  }
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
-  -- Telescope orthogonal deps
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
+	-- Telescope orthogonal deps
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
-  -- Lualine
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	-- Lualine
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  -- Tmux navigator
-  use 'christoomey/vim-tmux-navigator'
+	-- Tmux navigator
+	use("christoomey/vim-tmux-navigator")
 
-  -- File explorer
-  use {
-   'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    tag = 'nightly'
-  }
+	-- File explorer
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		tag = "nightly",
+	})
 
-  -- Maximizes and restores current window
-  use("szw/vim-maximizer")
+	-- Maximizes and restores current window
+	use("szw/vim-maximizer")
 
-  -- Add, delete, change surroundings
-  use("tpope/vim-surround")
+	-- Add, delete, change surroundings
+	use("tpope/vim-surround")
 
-  -- Commenting with gc
-  use("numToStr/Comment.nvim")
+	-- Commenting with gc
+	use("numToStr/Comment.nvim")
 
-  -- Autocompletion
-  use("hrsh7th/nvim-cmp") -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
+	-- Autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-  -- Snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-  use("rafamadriz/friendly-snippets") -- useful snippets
+	-- Snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
-  -- Managing & installing lsp servers, linters & formatters
-  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	-- Managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
-  -- Configuring lsp servers
-  use("neovim/nvim-lspconfig") -- easily configure language servers
-  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- Configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  -- Formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- Formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-  -- Git blame
-  use("f-person/git-blame.nvim")
+	-- Git blame
+	use("f-person/git-blame.nvim")
+
+	-- Highlight TODOs
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+	})
 end)
-
