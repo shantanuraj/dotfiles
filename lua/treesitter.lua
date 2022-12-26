373d322bdde01bd0631061423ca4e6046e25a63f
Shantanu Raj <s@sraj.me>
2022-12-26 22:19:50 +0100

    Format treesitter

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,11 +1,46 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "astro", "bash", "c", "cpp", "css", "dockerfile", "gitignore", "go", "gomod", "html", "http", "java", "javascript", "jsdoc", "json", "json5", "jsonc", "kotlin", "latex", "make", "markdown", "markdown_inline", "python", "regex", "ruby", "rust", "sql", "scss", "svelte", "toml", "tsx", "typescript", "vim", "vue", "yaml" },
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all"
+	ensure_installed = {
+		"astro",
+		"bash",
+		"c",
+		"cpp",
+		"css",
+		"dockerfile",
+		"gitignore",
+		"go",
+		"gomod",
+		"html",
+		"http",
+		"java",
+		"javascript",
+		"jsdoc",
+		"json",
+		"json5",
+		"jsonc",
+		"kotlin",
+		"latex",
+		"make",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"regex",
+		"ruby",
+		"rust",
+		"sql",
+		"scss",
+		"svelte",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"vue",
+		"yaml",
+	},
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-}
-
+	-- Automatically install missing parsers when entering buffer
+	auto_install = true,
+})
