f9f89aa1aa90deb4b528a91ca15b02362d4527d0
Shantanu Raj <s@sraj.me>
2023-04-14 23:59:48 +0200

    feat(nvim): Add context aware commenting for TS

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -91,7 +91,42 @@ return require("lazy").setup({
   "tpope/vim-surround",
 
   -- Commenting with gc
-  "numToStr/Comment.nvim",
+  {
+    "numToStr/Comment.nvim",
+    event = "BufRead",
+    dependencies = {
+      -- context aware commentstring for TypeScript
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      event = "VeryLazy",
+    },
+    config = function()
+      local opts = {
+        pre_hook = function(ctx)
+          -- Only for TypeScript React
+          if vim.bo.filetype == "typescriptreact" then
+            local U = require("Comment.utils")
+
+            -- Determine whether to use linewise or blockwise commentstring
+            local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
+
+            -- Determine the location where to calculate commentstring from
+            local location = nil
+            if ctx.ctype == U.ctype.blockwise then
+              location = require("ts_context_commentstring.utils").get_cursor_location()
+            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+              location = require("ts_context_commentstring.utils").get_visual_start_location()
+            end
+
+            return require("ts_context_commentstring.internal").calculate_commentstring({
+              key = type,
+              location = location,
+            })
+          end
+        end,
+      }
+      require("Comment").setup(opts)
+    end,
+  },
 
   -- Autocompletion
   "hrsh7th/nvim-cmp", -- completion plugin
