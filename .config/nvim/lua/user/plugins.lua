aa2713d47532c0a83b81d7da2bc87b4c3ae0206a
Shantanu Raj <s@sraj.me>
2023-04-15 17:08:41 +0200

    chore(nvim): Load lspsaga lazily

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -289,6 +289,18 @@ return require("lazy").setup({
       { "nvim-tree/nvim-web-devicons" },
       { "nvim-treesitter/nvim-treesitter" },
     },
+    opts = {
+      -- keybinds for navigation in lspsaga window
+      move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+      -- use enter to open file with finder
+      finder_action_keys = {
+        open = "<CR>",
+      },
+      -- use enter to open file with definition preview
+      definition_action_keys = {
+        edit = "<CR>",
+      },
+    },
   },
 
   -- Formatting & linting
