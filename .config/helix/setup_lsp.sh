6824d90b17badc08b9144ce325c947c613d8bc94
Shantanu Raj <s@sraj.me>
2023-06-23 14:21:30 +0200

    feat(helix): Add script to setup LSP servers
    - Lua, TS, HTML, CSS, Astro (not configured),
      Markdown, YAML
    - Go and Rust use gopls and rust-analyzer

---
+++ .config/helix/setup_lsp.sh
@@ -0,0 +1,6 @@
+# Install npm LSPs
+npm install -g \
+  typescript typescript-language-server vscode-langservers-extracted @astrojs/language-server
+
+# Install LSPs
+brew install lua-language-server marksman yaml-language-server 
