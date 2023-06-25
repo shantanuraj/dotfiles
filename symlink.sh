20de38462cadfdbd077179e0a96289daabbc5977
Shantanu Raj <s@sraj.me>
2023-06-26 01:49:33 +0200

    fix(symlink): gnu find doesn't support {file,directory}

--- symlink.sh
+++ symlink.sh
@@ -10,15 +10,15 @@ symlink() {
 	ln -svf "$PWD/$1" "$target"
 }
 
-/usr/bin/find . -mindepth 1 -maxdepth 1 \( -type file -o -type link \) \( -name '.*' -o -name 'blog' -o -name 'notes' \) \! -name '.gitmodules' | while read -r file; do
+/usr/bin/find . -mindepth 1 -maxdepth 1 \( -type f -o -type l \) \( -name '.*' -o -name 'blog' -o -name 'notes' \) \! -name '.gitmodules' | while read -r file; do
 	symlink "${file#./}"
 done
 
-/usr/bin/find . -mindepth 1 -maxdepth 1 -type dir -name '.*' \! -name '.config' \! -name '.git' | while read -r dir; do
+/usr/bin/find . -mindepth 1 -maxdepth 1 -type d -name '.*' \! -name '.config' \! -name '.git' | while read -r dir; do
 	symlink "${dir#./}"
 done
 
-/usr/bin/find .config -mindepth 1 -maxdepth 1 -type dir | while read -r dir; do
+/usr/bin/find .config -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
 	symlink "$dir"
 done
 
