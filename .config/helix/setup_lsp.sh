a57ee5c2d63e10c84e1636801984e4946feb01af
Shantanu Raj <s@sraj.me>
2023-06-25 14:31:27 +0200

    feat(brew): Record helix lsp deps
    - Add scrcpy

--- .config/helix/setup_lsp.sh
+++ .config/helix/setup_lsp.sh
@@ -1,6 +1,3 @@
 # Install npm LSPs
 npm install -g \
   typescript typescript-language-server vscode-langservers-extracted @astrojs/language-server
-
-# Install LSPs
-brew install lua-language-server marksman yaml-language-server 
