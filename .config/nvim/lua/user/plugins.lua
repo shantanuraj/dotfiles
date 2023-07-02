0819392ded5fc5c0f87b75935a6743f71604869c
Shantanu Raj <s@sraj.me>
2023-07-03 01:00:44 +0200

    feat(nvim): Add fancy splash
    - Configure some nice shortcuts
    - Revert 33d2786 not needed because of custom splash

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -84,7 +84,7 @@ return require("lazy").setup({
   -- Lualine
   {
     "nvim-lualine/lualine.nvim",
-    event = { "BufReadPost" },
+    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
     opts = function()
       local function show_macro_recording()
@@ -320,10 +320,9 @@ return require("lazy").setup({
     },
     config = require("user.lsp.lspconfig"),
   },
-
   {
     "nvimdev/lspsaga.nvim", -- enhanced lsp uis
-    event = { "BufReadPost" },
+    event = "LspAttach",
     branch = "main",
     dependencies = {
       { "nvim-tree/nvim-web-devicons" },
@@ -540,6 +539,56 @@ return require("lazy").setup({
           u = { "<cmd>UndotreeToggle<cr>", "Toggle undotree" },
         },
       })
+    end,
+  },
+
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = [[
+         ██╗   ██╗██╗███╗   ███╗          Z
+         ██║   ██║██║████╗ ████║      Z
+         ██║   ██║██║██╔████╔██║   z
+         ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+          ╚████╔╝ ██║██║ ╚═╝ ██║
+           ╚═══╝  ╚═╝╚═╝     ╚═╝
+    ]]
+
+      dashboard.section.header.val = vim.split(logo, "\n")
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files theme=get_ivy<CR>"),
+        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles only_cwd=true theme=get_ivy<CR>"),
+        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR> | :cd %:h/../.. <CR>"),
+        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+      }
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
     end,
   },
 })
