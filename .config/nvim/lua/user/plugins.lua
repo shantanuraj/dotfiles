43341f774c2f21ff890ea6c17bf443c01e051b96
Shantanu Raj <s@sraj.me>
2023-07-16 13:29:44 +0200

    chore(nvim): Use lazy keymap for undotree

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -527,17 +527,9 @@ return require("lazy").setup({
   -- Undo tree
   {
     "mbbill/undotree",
-    config = function()
-      local which_key_status, which_key = pcall(require, "which-key")
-      if not which_key_status then
-        return
-      end
-      which_key.register({
-        ["<leader>"] = {
-          u = { "<cmd>UndotreeToggle<cr>", "Toggle undotree" },
-        },
-      })
-    end,
+    keys = {
+      { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle undotree" },
+    },
   },
 
   {
