219f9e9e401c7c2ed1105f17041092fdc1ce33d9
Shantanu Raj <s@sraj.me>
2024-05-19 20:12:17 +0200

    feat(nvim): Add indent and usage text objects
    - `i` and `u`, based on LazyVim's mini.ai config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -383,6 +383,41 @@ return require("lazy").setup({
     event = "VeryLazy",
     dependencies = { "nvim-treesitter-textobjects" },
     opts = function()
+      local ai_indent = function(ai_type)
+        local spaces = (" "):rep(vim.o.tabstop)
+        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+        local indents = {} ---@type {line: number, indent: number, text: string}[]
+
+        for l, line in ipairs(lines) do
+          if not line:find("^%s*$") then
+            indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), text = line }
+          end
+        end
+
+        local ret = {} ---@type (Mini.ai.region | {indent: number})[]
+
+        for i = 1, #indents do
+          if i == 1 or indents[i - 1].indent < indents[i].indent then
+            local from, to = i, i
+            for j = i + 1, #indents do
+              if indents[j].indent < indents[i].indent then
+                break
+              end
+              to = j
+            end
+            from = ai_type == "a" and from > 1 and from - 1 or from
+            to = ai_type == "a" and to < #indents and to + 1 or to
+            ret[#ret + 1] = {
+              indent = indents[i].indent,
+              from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
+              to = { line = indents[to].line, col = #indents[to].text },
+            }
+          end
+        end
+
+        return ret
+      end
+
       local ai = require("mini.ai")
       return {
         n_lines = 500,
@@ -393,6 +428,9 @@ return require("lazy").setup({
           }, {}),
           f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
           c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+          i = ai_indent,
         },
       }
     end,
