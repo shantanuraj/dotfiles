ae631bd74153094a6386779abe63cf2202742202
Shantanu Raj <s@sraj.me>
2024-06-23 11:39:00 +0200

    fix(nvim): Trouble v3 configuration

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -675,25 +675,14 @@ return require("lazy").setup({
   -- Better diagnostics list and others
   {
     "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
+    cmd = { "Trouble" },
     keys = {
-      {
-        "gf",
-        function()
-          local trouble = require("trouble")
-          if trouble.is_open() then
-            trouble.close()
-          end
-          trouble.toggle("lsp_references")
-        end,
-        desc = "LSP references",
-      },
-      { "<leader>xt", "<cmd>TroubleToggle<cr>", desc = "Trouble" },
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+      { "gf", "<cmd>Trouble lsp toggle focus=false<cr>", desc = "LSP references" },
+      { "<leader>xt", "<cmd>Trouble close<cr>", desc = "Close (Trouble)" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Document Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace Diagnostics (Trouble)" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
       {
         "[q",
         function()
