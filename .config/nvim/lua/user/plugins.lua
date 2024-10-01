36c31e93cf7c4951a4e60904239d735c7b1a6311
Shantanu Raj <s@sraj.me>
2024-10-01 13:47:49 +0200

    chore(nvim): Unpin codecompanion seems to be fixed

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -38,7 +38,6 @@ return require("lazy").setup({
       "nvim-treesitter/nvim-treesitter",
       "nvim-telescope/telescope.nvim",
     },
-    commit = "abb1b8c6535e2a792b757ae7fb9253192735f5a8",
     config = function()
       require("codecompanion").setup({
         adapters = {
