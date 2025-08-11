5583d9534f1f5514dc463e8f76ab1d5dd845dfbe
Shantanu Raj <s@sraj.me>
2025-08-11 23:18:36 +0200

    feat(nvim): Renable illuminate

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -740,6 +740,37 @@ return require("lazy").setup({
   },
 
   {
+    "RRethy/vim-illuminate",
+    event = { "LspAttach" },
+    opts = { delay = 200, modes_denylist = { "i" } },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
+    end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
+    },
+  },
+
+  {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
@@ -833,26 +864,10 @@ return require("lazy").setup({
       scope = { enabled = true },
       scroll = { enabled = false },
       statuscolumn = { enabled = false },
-      words = { enabled = true },
+      words = { enabled = false },
       zen = { enabled = true },
     },
     keys = {
-      {
-        "]]",
-        function()
-          Snacks.words.jump(vim.v.count1)
-        end,
-        desc = "Next Reference",
-        mode = { "n", "t" },
-      },
-      {
-        "[[",
-        function()
-          Snacks.words.jump(-vim.v.count1)
-        end,
-        desc = "Prev Reference",
-        mode = { "n", "t" },
-      },
       {
         "<leader>.",
         function()
