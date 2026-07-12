5684d3b33473e7ca35f76a7f453c6269b2258722
Shantanu Raj <s@sraj.me>
2026-07-12 15:37:37 +0200

    feat(vi): Use default colorscheme
    - Use light background

--- .config/nvim-zen/init.lua
+++ .config/nvim-zen/init.lua
@@ -2,8 +2,6 @@ vim.g.mapleader = " "
 vim.g.netrw_list_hide = "^\\./\\=$"
 vim.g.netrw_banner = 0
 vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.cmd.colorscheme("quiet")
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
